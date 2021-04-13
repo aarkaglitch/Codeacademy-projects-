2a9a00afadde27289b86d82f7da91de35a10e18a
aarkaglitch <79043322+aarkaglitch@users.noreply.github.com>
2021-04-13 23:22:14 +0530

    Create kelvin1.js

---
+++ kelvin1.js
@@ -0,0 +1,10 @@
+/*todays temp*/
+let kelvin=297;
+/*temp in celcius*/
+let celcius=kelvin-293;
+/*temp in farenheit*/
+let fahrenheit=celcius*(9/5)+32;
+//rounding of fahrenheit 
+
+console.log("The temperature is "+Math.floor(fahrenheit)+" degrees Fahrenheit");
+
